762ce79db603ca84ff7dc410e124f6260bcc1992
Ziang Long <41099881+ZiangLong@users.noreply.github.com>
2018-11-15 14:20:38 -0800

    Create vgg.py

---
+++ vgg.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+class VGG(nn.Module):
+    def __init__(self, vgg_name):
+        super(VGG, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, 10)
+        
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+        
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        prev = in_channels
+        for cur in cfg:
+            if cur == 'M':
+                layers.append(nn.MaxPool2d(2, 2))
+            else:
+                layers.append(nn.Conv2d(prev, cur, 3, 1, 1))
+                layers.append(nn.BatchNorm2d(cur))
+                layers.append(nn.ReLU(True))
+                prev = cur
+        return nn.Sequential(*layers)
+            
+'''
+def test():
+    net = VGG('VGG11')
+    x = torch.randn(2,3,32,32)
+    y = net(x)
+    print(y.size())
+
+test()
+'''
